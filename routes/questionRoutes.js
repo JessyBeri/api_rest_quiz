ef056472dd840d5f726630864c6925869959dc85
Jessica Berichon <berichonjessica@gmail.com>
2023-09-14 13:43:20 +0400

    modif fonctions get ok, modif fonction post ok, modif fonction put en cours

--- routes/questionRoutes.js
+++ routes/questionRoutes.js
@@ -7,26 +7,34 @@ const router = express.Router(); // initialisation du router
 // router.get(), .post(), .put(), .delete()
 
 // récupérer toutes les questions, http://localhost:3000/questions
-router.get("/", (req, res) => {
+router.get("/", async (req, res) => {
     try {
-        console.log(req.body);
-        res.status(200).send(req.body);
+        const allQuestions = await Question.find();
+        res.status(200).send(allQuestions); // status 200 --> ok, request succeded
     } catch (err) {
-        res.status(400);
+        res.status(500).send(err); // status 500 --> Internal server error, The server has encountered a situation it does not know how to handle.
     }
 });
 
 // récupérer une question, http://localhost:3000/questions/:id
-router.get("/:id", (req, res) => {
+router.get("/:id", async (req, res) => {
     try {
-        res.status(200).send({ message: "la question avec id " + req.params.id + " est récupérée" });
+        const oneQuestion = await Question.findById(req.params.id);
+
+        // si aucun id (donc question) n'est trouvé --> erreur 404, sinon question récupérée
+        if (!oneQuestion) {
+            res.status(404).send({ error: "question non trouvée" });
+        } else {
+            res.status(200).send(oneQuestion);
+        }
     } catch (err) {
-        res.status(400);
+        res.status(500).send(err);
     }
 });
 
 // poster une question, http://localhost:3000/questions
 router.post("/", async (req, res) => {
+    // Méthode avec save()
     // const questionAsk = new Question(req.body);
 
     // try {
@@ -36,27 +44,28 @@ router.post("/", async (req, res) => {
     //     res.status(400).send(err);
     // }
 
-    //autre méthode
+    //Autre méthode avec create()
     try {
         const { question, multiple_choice } = req.body;
 
         const questionAsk = await Question.create({
             question,
-            multiple_choice
-        })
+            multiple_choice,
+        });
 
-        return res.status(201).send(questionAsk);
+        return res.status(201).send(questionAsk); // status 201 (created)
     } catch (error) {
-        return res.status(400).send({"error":error})
+        return res.status(400).send({ error: error }); // status 400 (bad request)
     }
 });
 
 // mettre à jour une question, http://localhost:3000/questions/:id
-router.put("/:id", (req, res) => {
+router.put("/:id", async (req, res) => {
     try {
-        res.status(200).send({ message: "la question avec id " + req.params.id + " est mise à jour" });
+        const updateQuestion = await Question.findByIdAndUpdate(req.params.id);
+        res.status(200).send(updateQuestion);
     } catch (err) {
-        res.status(400);
+        res.status(400).send(err);
     }
 });
 
